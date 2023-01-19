b541cd04e37de80074fa6a44a67c2b1e03926857
oduodg <werewildfire@gmail.com>
2023-01-19 20:54:09 +0900

    redux 활용해서 to do list 만들기 시작

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+// 3. action type을 상수로 선언하기
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+// 2. reducer 만들기
+// reducer는 state와 action을 받는다.
+// state의 기본 값으로 todo list를 담을 빈 배열을 넣어주자.
+const reducer = (state = [], action) => {
+  console.log(action)
+  switch(action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
+// 1. store 만들기
+// reducer를 만들어 준 후에 reducer를 넣어주자
+const store = createStore(reducer); 
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = e => {
+  e.preventDefault(); // submit을 클릭했을 때 새로고침 방지
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo});
 }
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
